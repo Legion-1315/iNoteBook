849f7c1ffb02471ec99ff29fc44d313f9e22c373
Legion-1315 <105158761+Legion-1315@users.noreply.github.com>
2022-10-15 16:29:12 +0530

    lec 53 completed

--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -50,4 +50,42 @@ router.post(
   }
 );
 
+
+// Route 2 : Add a new Notes using : POST"/api/notes/addnote". Login required
+router.put(
+  "/updatenote/:id",
+  fetchUser,
+  async (req, res) => {
+      const { title, description, tag } = req.body;
+      //create a newnote object
+      const newNote = {};
+      if (title) {
+          newNote.title = title;
+      }
+      if (description) {
+          newNote.description = description;
+      }
+      if (tag) {
+          newNote.tag = tag;
+      }
+
+      //find the note to be updated and update it after verifying that the user is actually the real user who has written the note.
+      let note = await Notes.findById(req.params.id);
+      
+      if (!note)
+      {
+          return res.status(404).send("Not Found");
+      }
+
+      if (note.user.toString() !== req.user.id)
+      {
+          return res.status(401).send("Not Allowed")
+      }
+
+      note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+      res.send(note);
+  }
+);
+
 module.exports = router;
